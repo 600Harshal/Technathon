4d0ecfc68d6a830b22eddaed9ce80341b3929bf7
Samruddhi Naukudkar <samruddhi_naukudkar@persistent.com>
2023-04-17 22:15:00 +0530

    prescription service

--- Frontend/src/components/PatientDashboard.js
+++ Frontend/src/components/PatientDashboard.js
@@ -1,15 +1,11 @@
 import React from "react";
-import data from "./dataSource.json";
-import {
-  GridComponent
 
-} from "@syncfusion/ej2-react-grids";
-import "./Style.css";
 
 const PatientDashboard = () => {
   return (
-    <div style={{ margin: "10%", marginTop: "5%", color: "black" }}>
-      <GridComponent dataSource={data}></GridComponent>
+    <div>
+
+
     </div>
   );
 };
