a98fc23d18b91b0b8a769f62ad992d09c8658522
KunalMohite <kunalmohite56@gmail.com>
2023-04-18 13:58:02 +0530

    Kunal changes

---
+++ Frontend/Kunal/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import DoctorSignup from "./Component/DoctorSignUp";
+import Login from "./Component/Login";
+import PatientSignUp from "./Component/PatientSignUp";
+import LandingPage from "./Component/LandingPage";
+import DemoLanding from "./Component/DemoLanding";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup/doctor" element={<DoctorSignup />} />
+          <Route path="/signup/patient" element={<PatientSignUp />} />
+        </Routes>
+      </Router>
+      {/* <LandingPage /> */}
+      {/* <Login /> */}
+      {/* <DoctorSignup /> */}
+      {/* <PatientSignUp /> */}
+    </>
+  );
+}
+
+export default App;
